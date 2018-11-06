1cf6431ed9091771d00477d072b08cee249be307
Gloriana Angulo Alvarado <g.angulo@b7-09.local>
2018-11-06 17:42:58 -0600

    Llamo mas elementos de la API

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,8 @@
 
 // Crea el request.
 const request = new XMLHttpRequest();
+const espacio = document.getElementById('space');
+const boton = document.getElementById('btn');
 
 // API de Films
 // Registra el manejador de eventos.
@@ -14,28 +16,35 @@ request.addEventListener('load', function (event) {
   console.log(response);
   // Itera sobre los resultados y los agrega a la lista.
   // Variables que contiene la informacion de la API
-  const itemResponseName = response[19].title;
-  const itemResponsePro = response[19].producer;
-  const itemResponse = response[19].description;
+  const itemResponseName = response[0].title;
+  const itemResponsePro = response[0].producer;
+  const itemResponseRelease = response[0].release_date;
+  const itemResponseScore = response[0].rt_score;
+  const itemResponse = response[0].description;
   
   // variable que crea el elemento HTML
   const container = document.getElementById('informacion');
-  const listItem = document.createElement('p');
   const listItemName = document.createElement('h1');
-  const listItemPro = document.createElement('h3');
-
+  const listItemPro = document.createElement('h4');
+  const listItemRelease = document.createElement('p');
+  const listItemScore = document.createElement('p');
+  const listItem = document.createElement('p');
   // Agrega la informacion al elemento HTML
-  listItem.innerHTML = itemResponse;
   listItemName.innerHTML = itemResponseName;
   listItemPro.innerHTML = itemResponsePro;
+  listItemRelease.innerHTML = `Fecha de lanzamiento: ${itemResponseRelease}`;
+  listItemScore.innerHTML = `  Puntuación: ${itemResponseScore }`;
+  listItem.innerHTML = itemResponse;
 
   // Agrega la informacion al elemento la pagina del navegador
   container.appendChild(listItemName);
   container.appendChild(listItemPro);
+  container.appendChild(listItemRelease);
+  container.appendChild(listItemScore);
   container.appendChild(listItem);
-  
 });
 
+// boton.onclick = ;
 // Define el tipo de respuesta.
 request.responseType = 'json';
 
