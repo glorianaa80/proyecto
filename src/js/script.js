55b4d8d585381b6aab223a1426307492ef0f0b24
Gloriana Angulo Alvarado <g.angulo@b7-09.local>
2018-11-08 13:36:48 -0600

    Hago la funcionalidad del buscador

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,70 @@
-/**
- * Obtiene una lista (parcial) de personajes de Rick and Morty.
- *
- */
-
 // Crea el request.
 const request = new XMLHttpRequest();
-const espacio = document.getElementById('space');
+ request.innerHTML = '';
+// Creacion de variables
 const boton = document.getElementById('btn');
 
+let response;
+
+function addelement() {
+  let element = document.getElementById('space').value;
+  console.log(element);
+  element = parseInt(element);
+ 
+  document.querySelector('h1').innerHTML = response[element].title;
+  document.querySelector('h4').innerHTML = response[element].director;
+  document.querySelector('#director').innerHTML = response[element].producer;
+  document.querySelector('#release').innerHTML = response[element].release_date;
+  document.querySelector('#score').innerHTML = response[element].rt_score;
+  document.querySelector('#description').innerHTML = response[element].description;
+
+}
 // API de Films
 // Registra el manejador de eventos.
+
 request.addEventListener('load', function (event) {
   // Obtiene la respuesta.
-  const response = event.target.response;
+  response = event.target.response;
   console.log(response);
   // Itera sobre los resultados y los agrega a la lista.
   // Variables que contiene la informacion de la API
-  const itemResponseName = response[0].title;
-  const itemResponsePro = response[0].producer;
-  const itemResponseRelease = response[0].release_date;
-  const itemResponseScore = response[0].rt_score;
-  const itemResponse = response[0].description;
-  
+  const itemResponseName = response.title;
+  const itemResponsePro = response.producer;
+  const itemResponseDi = response.director;
+  const itemResponseRelease = response.release_date;
+  const itemResponseScore = response.rt_score;
+  const itemResponse = response.description;
   // variable que crea el elemento HTML
   const container = document.getElementById('informacion');
   const listItemName = document.createElement('h1');
-  const listItemPro = document.createElement('h4');
+  const listItemPro = document.createElement('p');
+  listItemPro.setAttribute('id' , 'director')
+  const listItemDi = document.createElement('h4');
   const listItemRelease = document.createElement('p');
+  listItemRelease.setAttribute('id' , 'release')
   const listItemScore = document.createElement('p');
+  listItemScore.setAttribute('id' , 'score')
   const listItem = document.createElement('p');
+  listItem.setAttribute('id' , 'description')
   // Agrega la informacion al elemento HTML
   listItemName.innerHTML = itemResponseName;
+  listItemDi.innerHTML = itemResponseDi;
   listItemPro.innerHTML = itemResponsePro;
-  listItemRelease.innerHTML = `Fecha de lanzamiento: ${itemResponseRelease}`;
-  listItemScore.innerHTML = `  Puntuación: ${itemResponseScore }`;
+  listItemRelease.innerHTML = itemResponseRelease;
+  listItemScore.innerHTML = itemResponseScore;
   listItem.innerHTML = itemResponse;
-
   // Agrega la informacion al elemento la pagina del navegador
   container.appendChild(listItemName);
+  container.appendChild(listItemDi);
   container.appendChild(listItemPro);
   container.appendChild(listItemRelease);
   container.appendChild(listItemScore);
   container.appendChild(listItem);
 });
 
-// boton.onclick = ;
+
+
+boton.onclick = addelement;
 // Define el tipo de respuesta.
 request.responseType = 'json';
 
